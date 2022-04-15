a622bd5acc2cb6c7079495d97e07a7832ed1d76b
PauloKoglin <pk.koglin@gmail.com>
2022-04-15 16:01:52 +0200

    move type to domain and change fakeData generation

--- frontend/todo-list/src/app/components/todo-list/todo-list.component.ts
+++ frontend/todo-list/src/app/components/todo-list/todo-list.component.ts
@@ -1,36 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/domain/models/task';
 
-const fakeData = [
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true },
-  { id: 1, description: "my task 1", done: true }
+const getDescription = (): String => {
+  return `task ${new Date().toISOString()}`;
+}
+
+const fakeData: Task[] = [
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true },
+  { id: 1, description: getDescription(), done: true }
 ]
 
 @Component({
@@ -40,12 +45,15 @@ const fakeData = [
 })
 export class TodoListComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'task', 'done'];
-  tasks = fakeData;
+  tasks: Task[]
+  displayedColumns: string[] = ['id', 'task', 'done']
 
-  constructor() { }
+  constructor() {
+    this.tasks = fakeData
+   }
 
   ngOnInit(): void {
+
   }
 
 }
