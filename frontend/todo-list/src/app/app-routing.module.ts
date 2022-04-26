549dc1e0fac800aefc7b1fe16473d6e8570caea8
PauloKoglin <pk.koglin@gmail.com>
2022-04-26 19:47:38 +0200

    add routes to app

--- frontend/todo-list/src/app/app-routing.module.ts
+++ frontend/todo-list/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskComponent } from './views';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'tasks', component: TaskComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
