e89d816e448d34cd083bb16839912df105344077
PauloKoglin <pk.koglin@gmail.com>
2022-04-28 19:54:13 +0200

    add cards to dashboard

--- frontend/todo-list/src/app/views/dashboard/dashboard.component.ts
+++ frontend/todo-list/src/app/views/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/domain/models';
+import { TaskService } from 'src/app/services';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  tasks: Task[];
+  amountTasksDone: number;
+  amountTasksTodo: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly taskService: TaskService) {
+    this.tasks = [];
+    this.amountTasksDone = 0;
+    this.amountTasksTodo = 0;
   }
 
+  ngOnInit(): void {
+    this.taskService.getTasks()
+      .subscribe(tasks => {
+        this.tasks = tasks;
+
+        tasks.forEach(task => {
+          task.done && this.amountTasksDone++;
+          !task.done && this.amountTasksTodo++;
+        });
+      });
+    }
 }
