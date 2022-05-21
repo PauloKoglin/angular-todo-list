3bdff5ffd618b58a1fcc72f2045308c7e935600e
PauloKoglin <pk.koglin@gmail.com>
2022-05-21 08:28:11 +0200

    remove annotation from repository

--- backend/todolist/src/main/java/com/koglin/todolist/infra/database/repositories/TaskRepository.java
+++ backend/todolist/src/main/java/com/koglin/todolist/infra/database/repositories/TaskRepository.java
@@ -3,9 +3,7 @@ package com.koglin.todolist.infra.database.repositories;
 import com.koglin.todolist.infra.database.entities.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional
 public interface TaskRepository extends JpaRepository<TaskEntity, Integer> {
 }
