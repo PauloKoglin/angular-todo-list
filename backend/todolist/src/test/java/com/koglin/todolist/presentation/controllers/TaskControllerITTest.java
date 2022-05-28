88c05a3f4ed6ab932cce5817c6fea94c3f2d711b
PauloKoglin <pk.koglin@gmail.com>
2022-05-28 19:00:13 +0200

    ensure tasks getById returns status code 200 with correct response

--- backend/todolist/src/test/java/com/koglin/todolist/presentation/controllers/TaskControllerITTest.java
+++ backend/todolist/src/test/java/com/koglin/todolist/presentation/controllers/TaskControllerITTest.java
@@ -3,37 +3,29 @@ package com.koglin.todolist.presentation.controllers;
 import com.koglin.todolist.infra.database.entities.TaskEntity;
 import com.koglin.todolist.infra.database.repositories.TaskRepositoryJpa;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class TaskControllerITTest {
 
     @Autowired
     TaskRepositoryJpa repositoryJpa;
 
-    @BeforeAll
-    public void beforeAll() {
-        this.resetDatabase();
-    }
-
     private void insertTasksToDatabase() {
         repositoryJpa.saveAll(List.of(
-                new TaskEntity(null, "task 1", false),
-                new TaskEntity(null, "task 2", true)
+                new TaskEntity(1L, "task 1", false),
+                new TaskEntity(2L, "task 2", true)
         ));
-    }
-
-    private void resetDatabase() {
-        repositoryJpa.deleteAll();
     }
 
     @Test
@@ -65,6 +57,23 @@ public class TaskControllerITTest {
         final String expectedJson = "[]";
 
         client.get().uri("/tasks")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody().json(expectedJson);
+    }
+
+    @Test
+    public void getById_should_return_status_code_200_with_correct_response_body(@Autowired WebTestClient client) {
+        this.insertTasksToDatabase();
+        final String expectedJson = """
+                    {
+                        "id": 1,
+                        "description": "task 1",
+                        "done": false
+                    }
+                """;
+
+        client.get().uri("/tasks/1")
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody().json(expectedJson);
